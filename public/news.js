2096776f2138d9e4a39e2920320535a1b0ee87a8
qasemcoder <qasemcoder2020@gmail.com>
2020-05-30 02:04:39 +0300

    edit for page add news

--- public/news.js
+++ public/news.js
@@ -1,9 +1,11 @@
-if(localStorage.getItem('token') == undefined || localStorage.getItem('token') == null){
+if (localStorage.getItem('token') == undefined || localStorage.getItem('token') == null) {
     window.location.href = "../login/login.html";
 }
 
-let newNews= document.getElementById('newNews')
-let spanVald=document.getElementById('spanVald')
+let newNews = document.getElementById('newNews')
+let deloge = document.getElementById('deloge')
+let spanVald = document.getElementById('spanVald')
+
 function logout() {
     localStorage.removeItem('token')
     window.location.href = '../login/login.html'
@@ -16,25 +18,40 @@ let header = new Headers();
 header.append('content-type', 'application/json');
 
 function send_news() {
-    let newNews = document.getElementById('newNews')
-    console.log(newNews.value)
-    fetch(URL + 'addnews',{
-        method: 'post',
-        headers: header,
-        body: JSON.stringify({
-            newNews: newNews.value,
+    if (newNews.textLength == 0) {
+        deloge.style.display = 'block';
+        spanVald.innerHTML = 'Please Write New News';
+        newNews.value = ''
+    } else if (newNews.textLength < 10) {
+        deloge.style.display = 'block';
+        spanVald.innerHTML = 'Please Write long text';
+        newNews.value = ''
+    } else {
+        let newNews = document.getElementById('newNews')
+        console.log(newNews.value)
+        fetch(URL + 'addnews', {
+            method: 'post',
+            headers: header,
+            body: JSON.stringify({
+                newNews: newNews.value,
+            })
+        }).then(re => {
+            return re.json();
+        }).then(data => {
+            console.log(data)
+            if (data.status == 200) {
+                deloge.style.display = 'block';
+                spanVald.innerHTML = 'Done Insert New News';
+                newNews.value = ''
+            } else {
+                deloge.style.display = 'block';
+                spanVald.innerHTML = 'Wrong';
+                newNews.value = ''
+            }
         })
-    }).then(re => {
-        return re.json();
-    }).then(data => {
-        console.log(data)
-        if(data.status == 200) {
-            spanVald.innerHTML='حموزتي';
-            newNews.value=''
-        }
-    })
-    
-
+    }
 }
-
-
+//function close div deloge
+function closeMess() {
+    deloge.style.display = 'none';
+}
